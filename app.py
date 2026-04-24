51c578f72ef98ade90ee9927236e151a7e436dd9
PolBeby1488 <Polovkovyaroslav@gmail.com>
2026-04-24 23:33:01 +0300

    Added login and registration with styling

--- app.py
+++ app.py
@@ -1,18 +1,76 @@
-from flask import Flask
-from models import db
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, User
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandex_lyceum_secret_key_123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
-app.config['SECRET_KEY'] = 'my_super_secret_key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 with app.app_context():
     db.create_all()
 
+
 @app.route('/')
 def index():
-    return "База данных создана! Теперь можно делать регистрацию."
+    return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if User.query.filter_by(email=form.email.data).first():
+            flash('Этот email уже занят', 'danger')
+            return render_template('register.html', form=form)
+
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password_hash=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Регистрация успешна! Теперь войдите.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password_hash == form.password.data:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Неверный email или пароль', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Почта', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    confirm_password = PasswordField('Повторите пароль',
+                                    validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+class LoginForm(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
